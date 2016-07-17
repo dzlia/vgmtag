55564aeea52e6f5d141f86e817b381779b8e4623
Dźmitry Laŭčuk <>
2016-07-17 14:01:51 +0300

    The year span is updated in the program info.

--- src/main.cpp
+++ src/main.cpp
@@ -143,7 +143,7 @@ void printVersion()
 	}
 	const char * const authorPtr = author.c_str();
 	cout << PROGRAM_NAME << " " << PROGRAM_VERSION << "\n\
-Copyright (C) 2013-2015 " << authorPtr << ".\n\
+Copyright (C) 2013-2016 " << authorPtr << ".\n\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
